68185a96226cd1148bd1d8a0bd1c5e595998c33b
Sandeep Sreenath <sandeep.sreenath@gmail.com>
2022-12-04 04:20:50 +0530

    cleaned up text

--- vpay/telegram_bot.js
+++ vpay/telegram_bot.js
@@ -27,7 +27,9 @@ bot.command('pay', async (ctx) => {
 
     try {
         const msgArray = msg.split(' ')
+        const requestedUserHandle = msgArray[1]
         const requestedUser = msgArray[1].substr(1) // @codeswim -> codeswim
+
         const amount = msgArray[2]
 
         if (requestedUser in storedAddresses) {
@@ -36,10 +38,10 @@ bot.command('pay', async (ctx) => {
             const duneFormattedUrl = `https://dune.com/nccb/ETH-Address?1.+Eth+Address_t82ee2=${relatedAddress}`
 
             const a = async () => {
-                const outputString = `The current address linked with <b>${requestedUser}</b> is:`
+                const outputString = `The current address linked with <b>${requestedUserHandle}</b> is:`
                 await ctx.replyWithHTML(outputString)
                 await ctx.replyWithHTML(`<i>${relatedAddress}</i>`)
-                await ctx.replyWithHTML(`vPay has notified ${requestedUser} on their wallet address and they should respond back to you shortly on Telegram with the confirmation code: ${otp}`)
+                await ctx.replyWithHTML(`vPay has notified ${requestedUserHandle} on their wallet address and they should respond back to you shortly on Telegram with the confirmation code: <b>${otp}</b>`)
                 await ctx.replyWithHTML(`Meanwhile, checkout their account history and stats at ${duneFormattedUrl}`)
             }
             await a()
@@ -51,14 +53,14 @@ bot.command('pay', async (ctx) => {
                     type: 3, // target
                     identityType: 2, // direct payload
                     notification: {
-                        title: `vPay: Confirm Wallet Address`,
-                        body: `Your friend has requested confirmation of this wallet address. Respond with verification code: ${otp}`
+                        title: `Confirm Wallet Address`,
+                        body: `Your friend ${currentUserName} has requested confirmation of this wallet address. Respond with verification code: ${otp}`
                     },
                     payload: {
-                        title: `vPay: Confirm Wallet Address`,
-                        body: `Your friend has requested confirmation of this wallet address. Respond with verification code: ${otp}`,
+                        title: `Confirm Wallet Address`,
+                        body: `Your friend ${currentUserName} has requested confirmation of this wallet address. Respond with verification code: ${otp}`,
                         cta: `https://t.me/${currentUserName}`,
-                        img: 'https://dynamic.brandcrowd.com/preview/logodraft/6dd9e226-2497-41ae-a329-a38985bcdfc0/image/large.png'
+                        img: ''
                     },
                     recipients: pushFormattedReceiverAddress, // recipient address
                     channel: process.env.EPNS_CHANNEL_ID, // your channel address
@@ -69,13 +71,13 @@ bot.command('pay', async (ctx) => {
 
                 const chainId = 5
                 const metamaskLink = `https://metamask.app.link/send/pay-${relatedAddress}@${chainId}?value=${amount}e18`
-                await ctx.replyWithHTML(`To continue with the payment please click on this link: ${metamaskLink}`)
+                await ctx.replyWithHTML(`To continue with the payment after ${requestedUserHandle} confirms, click ${metamaskLink}`)
 
             } catch (err) {
                 console.error('Error: ', err);
             }
         } else {
-            const outputString = `${requestedUser} hasn't linked their wallet address. We have pinged ${requestedUser} to link it.`
+            const outputString = `${requestedUser} hasn't linked their wallet address. We have pinged ${requestedUserHandle} to link it.`
             ctx.replyWithHTML(outputString)
         }
     } catch (e) {
